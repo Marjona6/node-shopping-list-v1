0839f379f100d4307c74838d572cc22c4d8b05bb
Meryem Yanar <meryem.yanaryanar@gmail.com>
2017-07-04 22:41:53 +0300

    Add /recipes GET endpoint

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+'use strict';
 
-const express = require('express');
-const router = express.Router();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+var express = require('express');
+var router = express.Router();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+var _require = require('./models'),
+    ShoppingList = _require.ShoppingList;
 
-const jsonParser = bodyParser.json();
-const app = express();
+// creating the variable Recipes
+var Recipes = _require.Recipes;
+
+var jsonParser = bodyParser.json();
+var app = express();
 
 // log the http layer
 app.use(morgan('common'));
@@ -18,12 +23,24 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+// creating some items for Recipes
+// so there's something to get
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('pico de gallo', ['onions', 'jalapeno peppers', 'tomatoes', 'cilantro']);
+Recipes.create('rice pilaf', ['rice', 'oil', 'pasta', 'salt', 'butter']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
-app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
+app.get('/shopping-list', function (req, res) {
+    res.json(ShoppingList.get());
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+// when the root of this router is called with GET, return
+// all current Recipes items
+app.get('/recipes', function (req, res) {
+    res.json(Recipes.get());
+});
+
+app.listen(process.env.PORT || 8080, function () {
+    console.log('Your app is listening on port ' + (process.env.PORT || 8080));
 });
